6824b13c0c33af05ed09361805cf4da84389d8d6
Zeb Engberg <zebengberg@gmail.com>
2020-05-14 12:36:06 -0600

    implemented basic knn regressor

--- src/big_wall_prediction.py
+++ src/big_wall_prediction.py
@@ -2,24 +2,26 @@ import pandas as pd
 import numpy as np
 import xgboost as xgb
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.neighbors import KNeighborsRegressor
 from imblearn.over_sampling import SMOTE, SVMSMOTE, ADASYN, RandomOverSampler
+from prepare_big_wall_data import prepare_big_wall_data
 
 # Importing data
-explored = pd.read_csv('data/explored.csv')
-unexplored = pd.read_csv('data/unexplored.csv')
+accessible, inaccessible = prepare_big_wall_data()
+
 
 
 def build_x_y(prob=0.9):
   """Create balanced classes by oversampling."""
-  X_pred = unexplored.copy()  # to be used to make predictions
+  X_pred = inaccessible.copy()  # to be used to make predictions
 
-  X = explored.drop(columns=['latitude', 'longitude', 'mp_score'])
+  X = accessible.drop(columns=['latitude', 'longitude', 'mp_score'])
   mask = np.random.rand(len(X)) < prob
   X_train = X[mask]
   X_test = X[~mask]
 
   # For now, running classification models instead of regression.
-  y = explored.mp_score > 0
+  y = accessible.mp_score > 0
   y = y.map(lambda x: 1 if x else 0)
   y_train = y[mask]
   y_test = y[~mask]
@@ -30,68 +32,90 @@ def build_x_y(prob=0.9):
   return X_train, y_train, X_test, y_test, X_pred
 
 
-def run_lin_reg():
-  """Run a simple linear regression."""
+def run_linear():
+  """Train and predict with a simple linear regression."""
   X_train, y_train, X_test, y_test, X_pred = build_x_y()
-  reg = LinearRegression().fit(X_train, y_train)
-  print('Linear regression fit score: ' + str(reg.score(X_test, y_test)) + '\n')
-  y_pred = reg.predict(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
-  X_pred['predicted_score'] = y_pred
-  X_pred.height *= 1000
-  X_pred = X_pred.sort_values(by='predicted_score', ascending=False)[['latitude', 'longitude', 'height', 'mp_score', 'predicted_score']]
-  X_pred.to_csv('data/predictions/linear_reg_predictions.csv', header=True, index=False)
+  model = LinearRegression().fit(X_train, y_train)
+  print('Linear model fit score: {}\n'.format(model.score(X_test, y_test)))
+  y_pred = model.predict(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
 
-  print('Top 20 results from linear regression prediction.')
-  print('A higher score is better!\n')
-  print(X_pred[:20].to_string(index=False))
+  print_top_results(X_pred, y_pred, 'linear')
+  return y_pred
 
 
-def run_log_reg():
-  """Run a simple logistic classification."""
+def run_logistic():
+  """Train and predict with a simple logistic classification."""
   X_train, y_train, X_test, y_test, X_pred = build_x_y()
-  reg = LogisticRegression(max_iter=200).fit(X_train, y_train)
-  print('Logistic regression fit score: ' + str(reg.score(X_test, y_test)) + '\n')
+  model = LogisticRegression(max_iter=200).fit(X_train, y_train)
+  print('Logistic model fit score: {}\n'.format(model.score(X_test, y_test)))
   
   # Use predict_proba() to get probabilities of hittng a class, not classes themselves.
   # On the otherhand, predict() uses a cutoff at 0.5 to make a choice.
-  y_pred = reg.predict_proba(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
+  y_pred = model.predict_proba(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
   y_pred = y_pred[:, 1:]
+
+  print_top_results(X_pred, y_pred, 'logistic')
+  return y_pred
   
-  X_pred['predicted_score'] = y_pred
-  X_pred.height *= 1000
-  X_pred = X_pred.sort_values(by='predicted_score', ascending=False)[['latitude', 'longitude', 'height', 'mp_score', 'predicted_score']]
-  X_pred.to_csv('data/predictions/logistic_reg_predictions.csv', header=True, index=False)
-  
-  print('Top 20 results from logistic regression prediction.')
-  print('A higher score is better!\n')
-  print(X_pred[:20].to_string(index=False))
 
 def run_xgb():
+  """Train and predict with a boosted gradient tree."""
   X_train, y_train, X_test, y_test, X_pred = build_x_y()
   # dmatrix = xgb.DMatrix(data=X_train, label=y_train)
   # can also use XGBClassifier
-  reg = xgb.XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.1,
+  model = xgb.XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.1,
               max_depth = 5, alpha = 10, n_estimators = 10)
-  reg.fit(X_train, y_train)
+  model.fit(X_train, y_train)
   
-  y_pred = reg.predict(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
-  X_pred['predicted_score'] = y_pred
-  X_pred.height *= 1000
-  X_pred = X_pred.sort_values(by='predicted_score', ascending=False)[['latitude', 'longitude', 'height', 'mp_score', 'predicted_score']]
-  X_pred.to_csv('data/predictions/xgb_predictions.csv', header=True, index=False)
+  # Results
+  y_pred = model.predict(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
 
-  print('Top 20 results from xgb prediction.')
-  print('A higher score is better!\n')
-  print(X_pred[:20].to_string(index=False))
+  # Printing and returning
+  print_top_results(X_pred, y_pred, 'xgb')
+  return y_pred
 
 
+def run_knn(k=40):
+  """Predict with k-nearest neighbor regressor."""
+  X_train, y_train, X_test, y_test, X_pred = build_x_y(1)
+  model = KNeighborsRegressor(n_neighbors=k, weights='distance')
+  model.fit(X_train, y_train)
+
+  # Results
+  y_pred = model.predict(X_pred.drop(columns=['latitude', 'longitude', 'mp_score']))
+
+  # Printing and returning
+  print_top_results(X_pred, y_pred, 'knn')
+  return y_pred
 
 
-# nn regression
-# random forest
+def run_random_forest():
+  pass
+
+def run_neural_network():
+  pass
+
+def write_results():
+  pass
+  # eventually call something like:
+  # results.to_csv('../data/results.csv', header=True, index=False)
+
+
+def print_top_results(X, y, model):
+  """Print the inaccessible cliff formations with the highest predicted score."""
+  X['score'] = y
+  X.height *= 1000
+  X.sort_values(by='score', ascending=False, inplace=True)
+  X = X[['latitude', 'longitude', 'height', 'mp_score', 'score']]
+
+  print('Top 20 results from {} prediction.\n'.format(model))
+  print(X[:40].to_string(index=False))
+  print('\n' + '_' * 80)
+
 
 
 if __name__ == '__main__':
-  run_lin_reg()
-  run_log_reg()
-  run_xgb()
+  # run_linear()
+  # run_logistic()
+  # run_xgb()
+  run_knn()
