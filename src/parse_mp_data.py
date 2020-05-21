ff04c191504a52c82d88a22c36c9119ab061b5f2
Zeb Engberg <zebengberg@gmail.com>
2020-05-21 07:37:55 -0600

    stored MP area name in mp_data

--- src/parse_mp_data.py
+++ src/parse_mp_data.py
@@ -1,17 +1,18 @@
 """Parse data collected with mountain-project-scraper.
 See https://github.com/alexcrist/mountain-project-scraper."""
 
-import json
-import pandas as pd
 import random
+import json
 from collections import defaultdict
+import pandas as pd
 
-# Loading data scraped.
+
+print('Loading MP data.')
 with open('../mountain-project-scraper/clean-data.json') as file:
   data = json.load(file)  # scraped May 2020
 
 # data is a list; converting it to a dict to match format of children
-root = {'name': 'All Climbing', 'children': data}
+root = {'name': 'All Climbing', 'children': data, 'lat': 0, 'long': 0}
 
 # Notes on the data tree:
 #   - Each node in the data tree is either an area or a route.
@@ -24,16 +25,21 @@ root = {'name': 'All Climbing', 'children': data}
 
 def print_random_branch():
   """Print a random branch from the data tree."""
+  print('-' * 80)
   node = root
   depth = 0
   while 'children' in node and node['children']:
-    print(' ' * depth + node['name'])
+    line = ' ' * depth + node['name']
+    if 'lat' in node:
+      line += ' {} {}'.format(node['lat'], node['long'])
+    print(line)
     depth += 1
     node = random.choice(node['children'])
-  else:
-    print(' ' * depth + node['name'])
-    for key in node:
-      print(' ' * depth, key, node[key])
+
+  print(' ' * depth + node['name'])
+  for key in (key for key in node if key not in ['name', 'url']):
+    print(' ' * depth, key, node[key])
+  print('-' * 80)
 
 
 # Counting number of routes according to their types. Types of routes include:
@@ -43,12 +49,19 @@ def print_random_branch():
 boulder_dict = defaultdict(int)
 winter_dict = defaultdict(int)
 rock_dict = defaultdict(int)
-# Used to count total number of page views.
-views_dict = defaultdict(int)
+views_dict = defaultdict(int)  # counts total number of page views
+names_dict = {}  # using an ordinary dict here
 
 
 def dfs(node):
-  """Populate route_types_dict with DFS."""
+  """Populate dictionaries with DFS."""
+
+  # Using coordinates as key
+  key = (node['lat'], node['long'])
+  # Storing name of highest node in tree with unique key; any node below this
+  # one with the same key will be given this name.
+  if key not in names_dict:
+    names_dict[key] = node['name']
 
   # Sometimes node has 'children' property, but it is an empty list. This
   # if-statement avoids this situation.
@@ -58,10 +71,7 @@ def dfs(node):
       for child in node['children']:
         dfs(child)
 
-    # All children are leaf nodes, ie, routes!
-    else:
-      # Using parent coordinates as dictionary key
-      key = (node['lat'], node['long'])
+    else:  # all children are leaf nodes, ie, routes!
       for child in node['children']:
         if 'types' in child:
           types = child['types']
@@ -75,27 +85,29 @@ def dfs(node):
           views_dict[key] += child['totalViews']
 
 
-dfs(root)
 
+if __name__ == '__main__':
+  print('Parsing data.')
+  dfs(root)
 
-# Rarely is a key shared by all three dictionaries; taking the union here so we
-# can iterate over it to build pandas DataFrame.
-keys = set(boulder_dict.keys()).union(
-  set(winter_dict.keys())).union(set(rock_dict.keys()))
+  # Rarely is a key shared by all three dictionaries; taking the union here so we
+  # can iterate over it to build pandas DataFrame.
+  keys = set(boulder_dict.keys()).union(
+      set(winter_dict.keys())).union(set(rock_dict.keys()))
 
-# A list of dictionaries to be passed to a pandas DataFrame.
-pre_df = []
-for key in keys:
-  d = {
-    'latitude': key[0],
-    'longitude': key[1],
-    'num_boulders': boulder_dict[key],
-    'num_rock_routes': rock_dict[key],
-    'num_winter_routes': winter_dict[key],
-    'num_views': views_dict[key],
-    }
-  pre_df.append(d)
+  # A list of dictionaries to be passed to a pandas DataFrame.
+  pre_df = []
+  for k in keys:
+    d = {'latitude': k[0],
+         'longitude': k[1],
+         'num_boulders': boulder_dict[k],
+         'num_rock_routes': rock_dict[k],
+         'num_winter_routes': winter_dict[k],
+         'num_views': views_dict[k],
+         'name': names_dict[k]}
+    pre_df.append(d)
 
-# Exporting data.
-df = pd.DataFrame(pre_df)
-df.to_csv('../data/mp_data.csv', header=True, index=False)
+  # Exporting data.
+  df = pd.DataFrame(pre_df)
+  print('Writing parsed data to disk.')
+  df.to_csv('../data/mp_data.csv', header=True, index=False)
