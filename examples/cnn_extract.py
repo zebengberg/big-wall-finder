167a7ec7041bb9b1a753a866242c18f889d2dc7b
Zeb Engberg <zebengberg@gmail.com>
2021-02-06 08:01:59 -0700

    implement example cnn from earth engine patches

--- examples/cnn_extract.py
+++ examples/cnn_extract.py
@@ -6,18 +6,38 @@ import ee
 ee.Initialize()
 
 # small shards needed to avoid `computed value too large` errors
-N_SHARDS = 100
+N_TRAIN_SHARDS = 200
+N_EVAL_SHARDS = 50
 N_SAMPLES_PER_SHARD = 5000
 KERNEL_SIZE = 32
 
 
 # area of interest
-yosemite = ee.Geometry.Rectangle(
-    -119.67,
-    37.55,
-    -119.18,
-    37.84
-)
+xmin = -119.7
+xmax = -119.1
+ymin = 37.5
+ymax = 37.9
+delta_x = 0.01
+
+yosemite = ee.Geometry.Rectangle(xmin, ymin, xmax, ymax)
+coords = ee.List.sequence(ymin, ymax, 2 * delta_x)
+train_rectangles = coords.map(lambda coord: ee.Geometry.Rectangle(
+    ee.List([
+        xmin,
+        coord,
+        xmax,
+        ee.Number(coord).add(delta_x)
+    ])))
+train_geometry = ee.Geometry.MultiPolygon(train_rectangles)
+eval_rectangles = coords.map(lambda coord: ee.Geometry.Rectangle(
+    ee.List([
+        xmin,
+        ee.Number(coord).add(delta_x),
+        xmax,
+        ee.Number(coord).add(delta_x).add(delta_x)
+    ])))
+eval_geometry = ee.Geometry.MultiPolygon(eval_rectangles)
+
 
 naip = ee.ImageCollection('USDA/NAIP/DOQQ')
 naip = naip.filterBounds(yosemite)
@@ -53,19 +73,35 @@ def reduce_terrain(feature: ee.Feature):
   return feature.set({'slope': slope, 'aspect': aspect})
 
 
-for shard in range(N_SHARDS):
-  samples = image_array.sample(
-      region=yosemite,
-      scale=1,  # NAIP resolution
-      numPixels=N_SAMPLES_PER_SHARD
-  ).map(reduce_terrain)
+def make_shards(arg: str):
+  """Make train or eval shards."""
+  if arg == 'train':
+    n_shards = N_TRAIN_SHARDS
+    region = train_geometry
+  elif arg == 'eval':
+    n_shards = N_EVAL_SHARDS
+    region = eval_geometry
+  else:
+    raise ValueError('Unknown arg')
 
-  task = ee.batch.Export.table.toDrive(
-      # see https://gis.stackexchange.com/questions/324121/
-      # put folder 'yosemite_shards' anywhere in google drive
-      collection=samples,
-      folder='yosemite_shards',
-      description='yosemite_shard_' + str(shard),  # doubles as filename
-      fileFormat='TFRecord'
-  )
-  task.start()
+  for shard in range(n_shards):
+    samples = image_array.sample(
+        region=region,
+        scale=1,  # NAIP resolution
+        numPixels=N_SAMPLES_PER_SHARD
+    ).map(reduce_terrain)
+
+    task = ee.batch.Export.table.toDrive(
+        # see https://gis.stackexchange.com/questions/324121/
+        # put folder 'yosemite_shards' anywhere in google drive
+        collection=samples,
+        folder='yosemite_shards',
+        description='yosemite_' + arg + '_' + str(shard),  # filename
+        fileFormat='TFRecord'
+    )
+    task.start()
+
+
+if __name__ == '__main__':
+  make_shards('train')
+  make_shards('eval')
--- examples/cnn_model.py
+++ examples/cnn_model.py
@@ -1,3 +1,98 @@
 """Build CNN that predicts slope and aspect of region from NAIP pixels."""
 
+import os
+import glob
 import tensorflow as tf
+from tensorflow.keras import layers
+
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), 'yosemite_shards')
+TRAIN_RECORDS = glob.glob(DATA_PATH + '/*train*')
+EVAL_RECORDS = glob.glob(DATA_PATH + '/*eval*')
+FEATURES = ['R', 'G', 'B', 'N', 'slope', 'aspect']
+KERNEL_SIZE = 32
+COLUMNS = [
+    tf.io.FixedLenFeature(shape=(KERNEL_SIZE, KERNEL_SIZE), dtype=tf.float32),
+    tf.io.FixedLenFeature(shape=(KERNEL_SIZE, KERNEL_SIZE), dtype=tf.float32),
+    tf.io.FixedLenFeature(shape=(KERNEL_SIZE, KERNEL_SIZE), dtype=tf.float32),
+    tf.io.FixedLenFeature(shape=(KERNEL_SIZE, KERNEL_SIZE), dtype=tf.float32),
+    tf.io.FixedLenFeature([], dtype=tf.float32),
+    tf.io.FixedLenFeature([], dtype=tf.float32),
+]
+FEATURES_DICT = dict(zip(FEATURES, COLUMNS))
+
+
+@tf.function
+def convert_to_tuple(inputs):
+  """Helper function to convert features into an input, output tuple."""
+  feature_list = [inputs.get(key) for key in FEATURES[:4]]
+  stacked = tf.stack(feature_list, axis=0)
+  # Convert from CHW to HWC
+  stacked = tf.transpose(stacked, [1, 2, 0])
+  slope = inputs.get('slope') / 90
+  sin_aspect = tf.math.sin(inputs.get('aspect') / 360 * 2 * 3.14159)
+  cos_aspect = tf.math.cos(inputs.get('aspect') / 360 * 2 * 3.14159)
+  #target_list = [inputs.get(key) for key in FEATURES[4:]]
+  return stacked, [slope, sin_aspect, cos_aspect]
+
+
+def build_dataset(arg: str):
+  """Load, parse, and return tf dataset for train and eval TFRecords."""
+  if arg == 'train':
+    records = TRAIN_RECORDS
+  elif arg == 'eval':
+    records = EVAL_RECORDS
+  else:
+    raise ValueError
+
+  dataset = tf.data.TFRecordDataset(records, compression_type='GZIP')
+  dataset = dataset.map(
+      lambda inputs: tf.io.parse_single_example(inputs, FEATURES_DICT))
+
+  dataset = dataset.map(convert_to_tuple)
+  if arg == 'train':
+    return dataset.shuffle(10000).batch(16)
+  return dataset.batch(1)
+
+
+@tf.function
+def slope_loss(y_true, y_pred):
+  """Return absolute difference in slope angle coordinate."""
+  return tf.math.abs(y_true[:, 0] - y_pred[:, 0])
+
+
+@tf.function
+def aspect_loss(y_true, y_pred):
+  """Return angle between aspect vectors."""
+  dot = y_true[:, 1] * y_pred[:, 1] + y_true[:, 2] * y_pred[:, 2]
+  dot = tf.math.minimum(dot, 1.0)
+  dot = tf.math.maximum(dot, -1.0)
+  return tf.math.acos(dot)
+
+
+def build_model():
+  """Build CNN as tf Sequential."""
+  model = tf.keras.models.Sequential()
+  model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 4)))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+  model.add(layers.Flatten())
+  model.add(layers.Dense(64, activation='relu'))
+  model.add(layers.Dense(3))
+  model.compile(
+      optimizer='adam',
+      loss='mse',
+      metrics=['mae', slope_loss, aspect_loss]
+  )
+  return model
+
+
+if __name__ == '__main__':
+  train_dataset = build_dataset('train')
+  eval_dataset = build_dataset('eval')
+  model = build_model()
+  print(model.summary())
+  train_history = model.fit(train_dataset, epochs=10)
+  eval_history = model.evaluate(eval_dataset)
